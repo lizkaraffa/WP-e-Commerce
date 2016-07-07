8655e8ce7d0e319fa9195f9faa4ddaad46938602
Liz Karaffa <=>
2016-07-07 16:42:10 -0700

    wrapped product title in h3 to be more semantic

--- wpsc-components/theme-engine-v2/snippets/widgets/on-sale/widget.php
+++ wpsc-components/theme-engine-v2/snippets/widgets/on-sale/widget.php
@@ -14,7 +14,9 @@ if ( ! empty( $title ) )
 <?php 	endif; ?>
 
 <?php	if ( $instance['show_name'] ): ?>
-		<a class="wpsc-widget-product-title" href="<?php the_permalink(); ?>"><?php the_title(); ?></a>
+		<h3 class="wpsc-widget-product-title">
+			<a href="<?php the_permalink(); ?>"><?php the_title(); ?></a>
+		</h3>
 <?php endif; ?>
 
 <?php 	if ( $instance['show_description'] ): ?>
